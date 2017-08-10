29df4e38c69a33e743f8d3672a75c93b6907af0f
G N M <gorakhnath1992@gmail.com>
2017-08-10 18:31:59 +0530

    modal

---
+++ modal.php
@@ -0,0 +1,51 @@
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+<div class="container">  
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Place Order</h4>
+        </div>
+        <div class="modal-body">
+		<div class="form-group"  style="border:0px; padding-bottom:0px; margin:0;">
+          <select id="filter_vendor_id" name="filter_vendor_id" class="form-control">
+				<option value=""> Select Vendor </option>                   
+				<option value="1">Keshav,New Delhi</option>                  
+				<option value="2">Srikant,Bangalore</option>
+			</select>
+		</div>
+		<div class="form-group"  style="border:0px; padding-bottom:0px;margin:0;">
+          <select id="filter_vendor_id" name="filter_vendor_id" class="form-control">
+				<option value=""> Select Vendor </option>                   
+				<option value="1">Keshav,New Delhi</option>                  
+				<option value="2">Srikant,Bangalore</option>
+			</select>
+		</div>
+		
+		<div class="form-group"  style="border:0px; padding-bottom:0px;margin:0;">
+          <select id="filter_vendor_id" name="filter_vendor_id" class="form-control">
+				<option value=""> Select Vendor </option>                   
+				<option value="1">Keshav,New Delhi</option>                  
+				<option value="2">Srikant,Bangalore</option>
+			</select>
+		</div>
+		<div class="form-group">
+			<textarea rows="2" cols="" name="Notes-295470" id="Notes-295470" class="form-control" placeholder="Message"></textarea>
+		</div>
+		
+		<input type="button" value="Send" id="SubmitNotes-295470" name="Submit" class="btn btn-primary">
+		
+        </div>
+
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
+<a href="javascript:void(0);" class="" data-toggle="modal" data-target="#myModal">Place Order</a>
